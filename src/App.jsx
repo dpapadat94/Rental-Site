cb4dea97253e464435739c6a2eb2fae37d0f6a42
dpapadat94 <dennis.papadatos94@gmail.com>
2023-12-31 12:38:09 -0500

    NYE save

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./server";
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="about">About</Link>
+          <Link to="vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
