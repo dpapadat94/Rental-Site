cec47f2179594834b9cc06c8245395c50fbbc236
dpapadat94 <dennis.papadatos94@gmail.com>
2024-01-04 14:46:24 -0500

    outline finished

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -11,27 +11,27 @@ function HostLayout() {
     <>
       <nav className="host-nav">
         <NavLink
-          to="/host"
+          to="."
           end
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           DashBoard
         </NavLink>
         <NavLink
-          to="/host/income"
+          to="income"
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Income
         </NavLink>
         <NavLink
-          to="/host/vans"
+          to="vans"
           end
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Vans
         </NavLink>
         <NavLink
-          to="/host/reviews"
+          to="reviews"
           style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Reviews
--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,20 +1,66 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Outlet, useParams } from "react-router";
+import { NavLink, Link } from "react-router-dom";
 
 function HostVanDetail() {
-  const [van, setVan] = useState([]);
+  const [currentVan, setCurrentVan] = useState([]);
   const params = useParams();
+
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
 
   useEffect(() => {
     fetch(`/api/vans/${params.id}`)
       .then((res) => res.json())
-      .then((data) => setVan(data.vans));
+      .then((data) => setCurrentVan(data.vans));
   }, [params.id]);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
   return (
-    <div>
-      {van.name}
-      {van.id}
-    </div>
+    <section>
+      <Link to=".." className="back-button" relative="path">
+        &larr; <span>Back to all vans</span>
+      </Link>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+        <nav className="host-van-detail-nav">
+          <NavLink
+            to="."
+            end //to stop this link from being styles as active while another tab is selected
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Details
+          </NavLink>
+          <NavLink
+            to="pricing"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Pricing
+          </NavLink>
+          <NavLink
+            to="photos"
+            style={({ isActive }) => (isActive ? activeStyles : null)}
+          >
+            Photos
+          </NavLink>
+        </nav>
+        <Outlet context={{ currentVan }} />
+      </div>
+    </section>
   );
 }
 
